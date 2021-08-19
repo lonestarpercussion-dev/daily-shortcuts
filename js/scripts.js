e9ea9d9c4f4e3078467ec917349c8acca5696373
ahowley <howley.alex@gmail.com>
2021-08-19 10:51:16 -0500

    fixed uri encoding

--- js/scripts.js
+++ js/scripts.js
@@ -18,8 +18,6 @@ const searchUrlsByRecordType = {
     'Tooltip': 'https://665798.app.netsuite.com/app/common/search/ubersearchresults.nl?quicksearch=T&searchtype=Uber&frame=be&Uber_NAMEtype=KEYWORDSTARTSWITH&Uber_NAME=definition%3A',
 }
 
-const alnum = 'abcdefghijklmnopqrstuvwxyz1234567890';
-
 const adjustFieldWidth = () => {
     for (let section of document.getElementsByTagName('section')) {
         let inputFields = Array.from(section.getElementsByTagName('fieldset'));
@@ -75,21 +73,12 @@ const populateRecordTypes = () => {
 
 const searchWebsite = () => {
     let query = document.getElementById('website-query').value;
-    window.open('https://www.lonestarpercussion.com/#/~search/page/1/view/listView/keywords/' + query);
+    window.open('https://www.lonestarpercussion.com/#/~search/page/1/view/listView/keywords/' + encodeURIComponent(query));
 }
 
 const googSearch = () => {
     let query = document.getElementById('goog-query').value.toLowerCase();
-    let stringArray = [];
-    for (let letter of query) {
-        if (!alnum.includes(letter)) {
-            stringArray.push('+');
-        } else {
-            stringArray.push(letter);
-        }
-    }
-    query = stringArray.join('');
-    window.open('https://www.google.com/search?q=' + query);
+    window.open('https://www.google.com/search?q=' + encodeURIComponent(query));
 }
 
 const goToRecordById = () => {
@@ -102,7 +91,7 @@ const goToRecordById = () => {
 const searchRecord = () => {
     let recordType = document.getElementById('record-type-search').value;
     let searchQuery = document.getElementById('search-query').value;
-    window.open(searchUrlsByRecordType[recordType] + searchQuery);
+    window.open(searchUrlsByRecordType[recordType] + encodeURIComponent(searchQuery));
 }
 
 adjustFieldWidth();
